016bfef69e6f1d8b9524b1cf0ea89e80ade137fb
Brad Dworak <brad.dworak@gmail.com>
2013-01-30 15:45:11 -0600

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
-# Suppose for example your database consists of a collection of articles written by various authors published in numerous newspapers. You need to gather a list of all authors who published in a particular newspaper title called "The Chronicle". This search query/filter requires traversing the author|article|newspaper parent object relationships in Django. Assume you have a model where each Article has a foreign key to a certain newspaper and many-to-many relationships exist from authors to articles. Below is some example code on how to accomplish this.
 # Copyright 2013
 # Author: Brad Dworak
 
